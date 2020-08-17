2256ebd2a176f8ec3457584324566cdb1d7df6f3
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2020-08-17 12:35:10 +0530

    Update Transactions.java

--- src/model/Transactions.java
+++ src/model/Transactions.java
@@ -1,14 +1,13 @@
 package model;
 
 public class Transactions {
-	static int balance=0;
+	private static int balance=0;
 
 	public static int getBalance() {
 		return balance;
 	}
 
 	public void setBalance(int balance) {
-		System.out.println("i am setter method..i got 500");
-		this.balance = balance;
+		Transactions.balance = balance;
 	}
 }
